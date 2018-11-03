911cbe8eefdddce9cf18164d8be7544bf3137dd0
xuxiaoyin <279238850@qq.com>
2018-11-03 23:54:11 +0800

    change2

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,5 +1,17 @@
 <template>
-	<div>News组件</div>
+	<div>
+		<br>
+		<br>
+		<mt-button type="default" size="small">default</mt-button>
+		<br>
+		<br>
+		<mt-button type="primary" size="normal">primary</mt-button>
+		<br>
+		<br>
+		<mt-button type="danger" size="large">danger</mt-button>
+		<br>
+		<br>
+	</div>
 </template>
 
 <script>
